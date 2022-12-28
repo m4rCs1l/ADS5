415decbdf6eb1f7a0ef826ff93d7c09a0c163cab
m4rCs1l <112983175+m4rCs1l@users.noreply.github.com>
2022-12-28 18:17:24 +0300

    Add files via upload

---
+++ ads_last_1.py
@@ -0,0 +1,37 @@
+st = input('Введите последовательность скобок: ').strip()
+
+nums_o = []
+nums_c = []
+
+if st.count(')')+st.count('(') != len(st):
+    print('FUCK U')
+elif st[0] == ')':
+    print('NUMBA ONE')
+elif ')' not in st:
+    print('NUMBA ONE')
+else:
+    nums_o = [c for c in range(len(st)) if st[c] == '(']
+    nums_c = [z for z in range(len(st)) if st[z] == ')']
+    while len(nums_o) != 0 and len(nums_c) != 0:
+        if nums_o[0] < nums_c[0]:
+            nums_o.pop(0)
+            nums_c.pop(0)
+        else:
+            break
+    if len(nums_o) != 0 and len(nums_c) != 0:
+        print(f'NUMBA {min(nums_o[0], nums_c[0]) + 1} IS A GD FUCKER')
+    elif len(nums_o) == 0 and len(nums_c) == 0:
+        print(f'DA {st} IS A GOOD BOY')
+    else:
+        try:
+            print(f'NUMBA {nums_o[0] + 1} IS A GD FUCKER')
+        except IndexError:
+            print(f'NUMBA {nums_c[0] + 1} IS A GD FUCKER')
+
+
+
+
+
+
+
+
